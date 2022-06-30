a3a8d38fbad1c3d92f3799acf2023f41d3a3d829
ycassuto <ycassuto@cyber4s.dev>
2022-06-30 10:16:04 +0300

    add pokemon search

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,43 @@
 
 import { PokemonComponent } from "./shared/pokemonComponent";
 
-async function getPokemons() {
-    let allPokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000")
-        .then(res => res.json())
-        .then(data => data["results"]);
-    return allPokemons.map((pokemon: { name: string }) => pokemon.name)
+class Module {
+    pokemonsPromise: Promise<any>;
+
+    constructor() {
+        this.pokemonsPromise = this.getPokemons();
+    }
+
+    async getPokemons() {
+        let allPokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000")
+            .then(res => res.json())
+            .then(data => data["results"]);
+        return allPokemons.map((pokemon: { name: string }) => pokemon.name)
+    }
+
+
+    async getPokemonByName(name: string) {
+
+        let wantedPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+            .then(res => res.json())
+            .then(data => this.createPokemoneElement(data))
+            .catch(() => {/*TODO:ERROR MESSAGE */ })
+    }
+
+    createPokemoneElement(PokemonData:object){
+        console.log(PokemonData)
+    }
+
+    searchPokemon() {
+        let input = (<HTMLInputElement>document.getElementById("search-poke-input")).value;
+        this.getPokemonByName(input);
+    }
 }
 
 
-function getPokemonByName(name: string) {
-    getPokemons().then((pokemonsArrayNames) => {
-        pokemonsArrayNames.forEach((poke:string) => {
-            if (poke === name) {
-                console.log(poke);
-            }
-        });
-    })
-}
 
-getPokemonByName("alakazam")
+export const module = new Module();
+
+document.addEventListener("load", () => {
+    module.getPokemons();
+})
